9719f6e20832bb1ed19f338a5f2b242e4f31eeeb
abhishek <abhirajabhi312@gmail.com>
2023-11-14 00:11:10 +0530

    started adding models for user and videos

---
+++ yt_backend_course/backend_project-1/src/model/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+
+//schema starts here !
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //here cloudinary url will come
+      required: true,
+    },
+    coverImage: {
+      type: String, //same cloudinary url will come here
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//exporting the schema
+export const User = mongoose.model("User", userSchema);
