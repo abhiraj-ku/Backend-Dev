919b2b20dcd4288a9122e8ae5baa2f94016fc712
abhishek <abhirajabhi312@gmail.com>
2023-11-18 19:22:13 +0530

    used middleware

--- Backend_Course/formAndImg/index.js
+++ Backend_Course/formAndImg/index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.get("/", (req, res) => {
+  // console.log("hello from abhi");
+  res.send("hello");
+});
+
+app.get("/getit", (req, res) => {
+  console.log(req.body);
+
+  res.send(req.body);
+});
+app.listen(6000, () => {
+  console.log(`server is running at port 6000`);
+});
